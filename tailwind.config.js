593d7a283fe0e06a3bc6ac42cb41491b6b4379a7
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2023-09-20 02:12:10 +0700

    header version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'content': {
+          100: '#E6E6E6',
+          400: '#999999',
+          500: '#808080',
+          600: '#666666',
+          700: '#4D4D4D'
+        },
+        'success': '#00B207',
+      },
       fontFamily: {
         Poppins: ['poppins', 'sans-serif']
       }
